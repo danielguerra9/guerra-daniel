e8bf31fe836641a7e2b66c50001136c1786ce77a
Daniel Guerra <106002019+danielguerra9@users.noreply.github.com>
2023-09-04 23:29:56 -0500

    Add files via upload

---
+++ Lab2/Lab2.1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 2.1 </title>
+</head>
+<body>
+    <?php  include ("lab21_header.php");?>
+    <p>
+        Hola , este es el contenido
+</p>
+<?php include("lab21_footer.php");?>
+</body>
+</html>
---
+++ Lab2/Lab2.2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 2.2</title>
+</head>
+<body>
+    <?php 
+    include ("no existe.php");
+    echo ("Hola . El scrip siguio!");
+    ?>
+    
+</body>
+</html>
