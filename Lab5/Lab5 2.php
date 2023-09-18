3a7054da6257396d80a0668153db5a0a7d0e2de3
Daniel Guerra <106002019+danielguerra9@users.noreply.github.com>
2023-09-17 19:09:41 -0500

    Add files via upload

---
+++ Lab5/Lab5 2.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('upload_max_filesize','1M');
+ini_set('Post_max_size','1M');
+
+
+ if (is_uploaded_file($_FILES['nombre_archivo_cliente']['tmp_name']))
+ {
+    $nombreDirectorio = "archivo/";
+    $nombrearchivo = $_FILES['nombre_archivo_cliente']['name'];
+    $nombreCompleto = $nombreDirectorio . $nombrearchivo;
+    if(is_file($nombreCompleto))
+    {
+        $idUnico = time();
+        $nombreArchivo = $idUnico . "-" . $nombrearchivo;
+        echo "Archivo repetido, se cambiara el nombre a $nombrearchivo 
+        <br><br>";
+        
+    }
+    move_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'],
+    $nombreDirectorio . $nombrearchivo);
+    echo "El archivo se ha subido sactifactoriamente al directorio
+    $nombreDirectorio <br>";
+ }
+ else 
+ echo "No se ha podido subir el archivo <br>";
+?>
